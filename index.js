fc15500e23cf6f02dc47dac7c3584cddc5321950
Mehri Russo <mehrimova@gmail.com>
2016-11-27 14:35:35 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const knex = require('knex')(config);
 
 knex('trails')
 .select('trails.name', 'conditions.condition')
-
 // INNER JOIN
 .from('trails').innerJoin('conditions', 'trails.id', 'conditions.trail_id')
 // .where('name', 'Grays Peak')
@@ -21,7 +20,3 @@ knex('trails')
 });
 
 // NOTE: Inner Join: SELECT * FROM Trails INNER JOIN Conditions ON Trails.id = Conditions.trail_id
-
-// console.log(sql);
-
-// knex.destroy();
